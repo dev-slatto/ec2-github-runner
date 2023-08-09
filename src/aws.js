bbea597ab33eb03132d313076e80eb82acddc7df
Pål-Magnus Slåtto <pm@pusterom.as>
2023-08-09 11:50:13 +0200

    Map Public IP to instance

--- src/aws.js
+++ src/aws.js
@@ -47,6 +47,9 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    NetworkInterfaces: [{
+      AssociatePublicIpAddress: config.input.associatePublicIpAddress
+    }]
   };
 
   try {
