f436a6e136c55f63c89a648ffa05c4e4e9a688cb
Pål-Magnus Slåtto <paal.magnus@favrit.com>
2023-08-09 12:11:04 +0200

    Parse as boolean

--- src/aws.js
+++ src/aws.js
@@ -48,7 +48,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
     NetworkInterfaces: [{
-      AssociatePublicIpAddress: config.input.associatePublicIpAddress
+      AssociatePublicIpAddress: Boolean(config.input.associatePublicIpAddress)
     }]
   };
 
